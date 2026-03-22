70720ac60970c34319df12b493b65a1d59fc83d2
chk <2046420660@qq.com>
2026-03-22 14:24:38 +0800

    P8 设置界面后端

--- my-project-backend/src/main/java/com/example/controller/AccountController.java
+++ my-project-backend/src/main/java/com/example/controller/AccountController.java
@@ -2,11 +2,18 @@ package com.example.controller;
 
 import com.example.entity.RestBean;
 import com.example.entity.dto.Account;
+import com.example.entity.dto.AccountDetails;
+import com.example.entity.vo.request.DetailsSaveVO;
+import com.example.entity.vo.response.AccountDetailsVO;
 import com.example.entity.vo.response.AccountVO;
+import com.example.service.AccountDetailsService;
 import com.example.service.AccountService;
 import com.example.utils.Const;
 import jakarta.annotation.Resource;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
@@ -15,9 +22,27 @@ public class AccountController {
     @Resource
     AccountService accountService;
 
+    @Resource
+    AccountDetailsService accountDetailsService;
+
     @GetMapping("/info")
     public RestBean<AccountVO> findAccountById(@RequestAttribute(Const.ATTR_USER_ID) int id){
         Account account = accountService.findAccountById(id);
         return account == null ? RestBean.failure(401, "用户信息请求失败") : RestBean.success(account.asViewObject(AccountVO.class));
+    }
+
+    @GetMapping("/details")
+    public RestBean<AccountDetailsVO> details(@RequestAttribute(Const.ATTR_USER_ID) int id){
+        AccountDetails details = Optional
+                .ofNullable(accountDetailsService.findAccountDetailsById(id))
+                .orElseGet((AccountDetails::new));
+        return RestBean.success(details.asViewObject(AccountDetailsVO.class));
+    }
+
+    @PostMapping("/save-details")
+    public RestBean<Void> saveDetails(@RequestAttribute(Const.ATTR_USER_ID) int id,
+                                      @RequestBody @Validated DetailsSaveVO vo){
+        boolean success = accountDetailsService.saveAccountDetails(id, vo);
+        return success ? RestBean.success() : RestBean.failure(400, "此用户名已被其他用户使用，请重新更换！");
     }
 }
---
+++ my-project-backend/src/main/java/com/example/service/impl/AccountDetailsServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.entity.dto.Account;
+import com.example.entity.dto.AccountDetails;
+import com.example.entity.vo.request.DetailsSaveVO;
+import com.example.mapper.AccountDetailsMapper;
+import com.example.service.AccountDetailsService;
+import com.example.service.AccountService;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountDetailsServiceImpl extends ServiceImpl<AccountDetailsMapper, AccountDetails> implements AccountDetailsService {
+
+    @Resource
+    AccountService service;
+
+    @Override
+    public AccountDetails findAccountDetailsById(int id) {
+        return this.getById(id);
+    }
+
+    @Override
+    public synchronized boolean saveAccountDetails(int id, DetailsSaveVO vo) {
+        Account account = service.findAccountByNameOrEmail(vo.getUsername());
+        if (account == null || account.getId() == id) {
+
+            boolean updated = service.update()
+                    .set("username", vo.getUsername())
+                    .eq("id", id)
+                    .update();
+
+            boolean detailsSaved = this.saveOrUpdate(
+                    new AccountDetails(id, vo.getGender(), vo.getPhone(), vo.getQq(), vo.getWx(), vo.getDesc())
+            );
+
+            return updated && detailsSaved;
+        }
+        return false;
+    }
+}
